3272f71ac0c57f32215bcb23bc0f1bb26d3aeafd
이성일 <kw8989@naver.com>
2021-03-23 10:23:24 +0900

    stduy8-git

--- src/components/study_8.js
+++ src/components/study_8.js
@@ -6,9 +6,18 @@ function Study_8({data, property}) {
     const svgRef = useRef();
     const wrapperRef = useRef();
     const dimensions = useResizeObserver(wrapperRef);
-    const [selectedCountry, setSelectedCountry] = useState(null)
+    const [selectedCountry, setSelectedCountry] = useState(null);
+
+    const [p, setp]= useState(null)
 
     useEffect(() => {
+
+
+        console.log(p);
+
+
+
+
         const svg = select(svgRef.current);
         // console.log("1 : ",svgRef); // tag 이름
         // console.log("2 : ",svgRef.current);  // tag 전체 선택
@@ -22,36 +31,51 @@ function Study_8({data, property}) {
 
         const {width, height} =
         dimensions || wrapperRef.current.getBoundingClientRect();
-        console.log("data : ", data)
-        console.log("data.features : ", data.features)
-        console.log("feature : ", feature => feature)
-        console.log("feature.properties[property] : ", feature => feature.properties[property])
-        console.log("minProp : ", minProp)
-        console.log("maxProp : ", maxProp)
-        console.log("colorScale : ", colorScale)
-        console.log("dimensions : ", dimensions)
-        console.log("width  height : ", width, height)
+        // console.log("data : ", data)
+        // console.log("data.features : ", data.features)
+        // console.log("feature : ", feature => feature)
+        // console.log("feature.properties[property] : ", feature => feature.properties[property])
+        // console.log("minProp : ", minProp)
+        // console.log("maxProp : ", maxProp)
+        // console.log("colorScale : ", colorScale)
+        // console.log("dimensions : ", dimensions)
+        // console.log("width  height : ", width, height)
 
-        const projection = geoMercator()
-            .fitSize([width, height], selectedCountry || data)
-            .precision(100);
+        const projection = geoMercator().fitSize(
+            // [width, height], selectedCountry || data
+            [width, height], selectedCountry || data
+        ).precision(100);
 
         const pathGenerator = geoPath().projection(projection);
 
-        console.log("pathGenerator : ", pathGenerator)
+        console.log("selectedCountry : ",selectedCountry);
+        console.log("projection : ", projection)
+
+
+        // this.testRef.current.addEventListener(CLICK, ()=> (this.props.data))
+        //
+        // onClick={this.handleChartClick}
+        //
+        // handleChartClick = (data) =>{
+        //     console.log('data length on click', data);
+        // }
 
         svg
             .selectAll(".country")
             .data(data.features)
             .join("path")
-            .on("click", feature => {
+            .on("click", (e,feature) => {
                 setSelectedCountry(selectedCountry === feature ? null : feature);
+                // const copiedObj = Object.assign(feature);
+                // console.log(copiedObj);
+                // setSelectedCountry(123123);
+                // console.log("setCountry :",selectedCountry);
             })
             .attr("class", "country")
             .transition()
+            .duration(1000)
             .attr("fill", feature => colorScale(feature.properties[property]))
             .attr("d", feature => pathGenerator(feature));
-
 
         // svg
         //     .selectAll(".label")
